d35a40bb23ccfd23f383bdb1164f70d918058094
Julia Linek <leeenek0@gmail.com>
2022-05-06 20:09:19 +0200

    Change service

--- Service/service.ts
+++ Service/service.ts
@@ -13,24 +13,22 @@ import Event from '../models/Event';
 
 export const secret = 'LubiePieski123';
 
-export class Service {
-  public async updateStorage(
-    storeFile: string,
-    data: Center | Dog | Event | User
-  ): Promise<void> {
-    try {
-      await fs.promises.writeFile(storeFile, JSON.stringify(data));
-    } catch (err) {
-      console.log(err);
-    }
+export async function updateStorage(
+  storeFile: string,
+  data: Center | Dog | Event | User
+): Promise<void> {
+  try {
+    await fs.promises.writeFile(storeFile, JSON.stringify(data));
+  } catch (err) {
+    console.log(err);
   }
+}
 
-  public async readStorage(file: string): Promise<void> {
-    try {
-      const data = await fs.promises.readFile(file, 'utf-8');
-      return JSON.parse(data);
-    } catch (err) {
-      console.log(err);
-    }
+export async function readStorage(file: string): Promise<Center[] | Dog[] | Event[] | User[]> {
+  try {
+    const data = await fs.promises.readFile(file, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(err);
   }
 }
--- routing/UserRouting.ts
+++ routing/UserRouting.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express'
+import { readStorage } from '../service/service';
 
 const express = require('express');
 const router = express.Router();
 const app = express();
 app.use(express.json());
 
-router.get('/:id', (req: Request, res: Response) =>{
+const storeUsersFile = '../AdoptionCenter/Data/storeUsers.json';
 
+router.get('/:id', async (req: Request, res: Response) =>{
+  // ToDo: jeśli zalogowany użytkownik jest adminem
+  const users = await readStorage(storeUsersFile);
 });
 
 module.exports = router;
