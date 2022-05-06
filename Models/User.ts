c6cc56ad02b99b61e4b101c1c0d8cb06c7d6eb69
Julia Linek <leeenek0@gmail.com>
2022-05-06 19:16:33 +0200

    Models (User), arrays

---
+++ Models/User.ts
@@ -0,0 +1,16 @@
+class User{
+  public Id: number;
+  public Login: string;
+  public Name: string;
+  public IsAdmin: boolean = false;
+  public Events?: Event[];
+  public Surname: string;
+
+  constructor(user: User){
+    this.Login = user.Login;
+    this.Name = user.Name;
+    this.IsAdmin = user.IsAdmin;
+    this.Surname = user.Surname;
+  }
+}
+export default User;
---
+++ Service/service.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import Center from '../Models/Center';
+import Dog from '../Models/Dog';
+import User from '../Models/User';
+
+export let centers: Center[] = [];
+export let dogs: Dog[] = [];
+export let events: Event[] = [];
+export let users: User[] = [];
