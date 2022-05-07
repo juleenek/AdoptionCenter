8903f83a6aec1284005d36ecdd90cfc10be8424a
Konrad Barszcz <konrad.barszcz@microsoft.wsei.edu.pl>
2022-05-07 19:52:57 +0200

    Fixed Center(POST), Working on Center(PUT)

--- routes/center.ts
+++ routes/center.ts
@@ -4,6 +4,7 @@ import Dog from '../models/Dog'
 import Event from '../models/Event'
 import { updateStorage, readStorage } from '../service/service'
 import { checkRequired } from '../Service/checks';
+import { registerCenterValidation } from '../validation'
 const express = require('express');
 const uniqid = require('uniqid');
 const router = express.Router();
@@ -30,18 +31,50 @@ router.get('/:id', async (req: Request, res: Response) =>{
 
 //POST ADD NEW CENTER(ADMIN)
 router.post('', async (req: Request, res: Response) =>{
-    const centers = await readStorage(CenterPath);
+    const centers: any = await readStorage(CenterPath);
     const center: Center = req.body;
 
-    checkRequired(center.centerName, res, 'Please enter a center name.', 400);
-    checkRequired(center.city, res, 'Please enter a center city.', 400);
-    checkRequired(center.address, res, 'Please enter a center address.', 400);
-    checkRequired(center.phone, res, 'Please enter a center phone number.', 400);
-    checkRequired(center.password, res, 'Please enter a center password.', 400);
+    const name = (center.centerName = center.centerName.toLowerCase());
+    const findSameName = centers.find(((x: any) => x.centerName == name))
 
-    //const name = (center.CenterName = center.CenterName.toLowerCase());
+    const { error } = registerCenterValidation(center);
+    if (error) return res.status(400).send('Invalid data, try again.');
+
+    if(findSameName){
+    return res.status(400).send("This center already exists.");
+    }
+    else{
     center.id = uniqid();
     await updateStorage(CenterPath, [...centers, center]);
-    res.status(201).send(center);
-}) 
+    return res.status(201).send(center);
+    }
+})
+
+//PUT UPDATE CENTER DATA(ADMIN)
+router.put('/:id', async(req: Request, res: Response) =>{
+    const centers: any = await readStorage(CenterPath);
+    const newCenter: Center = req.body;
+    const oldCenter = centers.find((center: any) => center.id === req.params.id)
+
+    const name = (newCenter.centerName = newCenter.centerName.toLowerCase());
+    const findSameName = centers.find(((x: any) => x.centerName == name))
+
+    const centerIndex: number = centers.findIndex((n: any) => n.id === req.params.id)
+    const { error } = registerCenterValidation(newCenter);
+
+    if (error) return res.status(400).send('Invalid data, try again.');
+    if(oldCenter == undefined){
+    return res.status(404).send("This center doesn't exist.");
+    }
+
+    if(findSameName && req.params.id != oldCenter.id){
+        console.log(centerIndex);
+        res.status(404).send("This center doesn't exist.");
+    }
+    else{
+        console.log(centerIndex);
+        await updateStorage(CenterPath, [...centers, centers[centerIndex] = newCenter]);
+        return res.status(201).send(newCenter);
+    }
+})
 module.exports = router;
