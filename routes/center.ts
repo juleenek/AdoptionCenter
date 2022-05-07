cd9a247b25375a24d7eecde8fd600e6a350ff933
Julia Linek <leeenek0@gmail.com>
2022-05-07 19:51:48 +0200

    Working on a token

--- routes/center.ts
+++ routes/center.ts
@@ -3,7 +3,6 @@ import Center from '../models/Center'
 import Dog from '../models/Dog'
 import Event from '../models/Event'
 import { updateStorage, readStorage } from '../service/service'
-import { checkRequired } from '../Service/checks';
 const express = require('express');
 const uniqid = require('uniqid');
 const router = express.Router();
@@ -32,12 +31,6 @@ router.get('/:id', async (req: Request, res: Response) =>{
 router.post('', async (req: Request, res: Response) =>{
     const centers = await readStorage(CenterPath);
     const center: Center = req.body;
-
-    checkRequired(center.centerName, res, 'Please enter a center name.', 400);
-    checkRequired(center.city, res, 'Please enter a center city.', 400);
-    checkRequired(center.address, res, 'Please enter a center address.', 400);
-    checkRequired(center.phone, res, 'Please enter a center phone number.', 400);
-    checkRequired(center.password, res, 'Please enter a center password.', 400);
 
     //const name = (center.CenterName = center.CenterName.toLowerCase());
     center.id = uniqid();
