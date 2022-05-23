fc0bddcf2d85884425db326ba8dbb8467920fe4b
Julia Linek <leeenek0@gmail.com>
2022-05-23 14:51:01 +0200

    User DELETE

--- routes/user/user.ts
+++ routes/user/user.ts
@@ -44,6 +44,7 @@ router.put(
 
     const users: User[] = await readStorage(storeUsersFile);
 
+    // If there is a user with the given ID
     if (users.some((user) => user.id === req.params.id)) {
       let oldUser = users.find((user) => user.id === req.params.id) as User;
       const newUser: User = req.body;
@@ -54,25 +55,67 @@ router.put(
       newUser.id = req.params.id;
       newUser.role = oldUser.role;
 
+      // If data has not been changed
       if (_.isEqual(oldUser, newUser))
         return res.status(400).send('The data is the same as before.');
 
+      // If logged in user is admin
       if (decodedUser.role == 'admin') {
         oldUser = Object.assign(oldUser, newUser);
         updateStorage(storeUsersFile, users);
         res.status(201).send(newUser);
       }
       if (decodedUser.role == 'user') {
+        // If logged in user changes his own data
         if (decodedUser.id == newUser.id) {
           oldUser = Object.assign(oldUser, newUser);
           updateStorage(storeUsersFile, users);
           res.status(201).send(newUser);
         } else {
-          return res.status(400).send('You can\'t change data that isn\'t yours.');
+          return res
+            .status(400)
+            .send("You can't change data that isn't yours.");
         }
       }
     } else {
-      return res.status(400).send('User with that id doesn\'t exist.');
+      return res.status(400).send("User with that id doesn't exist.");
+    }
+  }
+);
+
+router.delete(
+  '/:id',
+  authentication,
+  requiresUserAdmin,
+  async (req: Request, res: Response) => {
+    const authorizationHeader = req.headers.authorization as string;
+    const token = authorizationHeader.split(' ')[1];
+    const decodedUser: User = jwt.decode(token) as JwtPayload as User;
+
+    const users: User[] = await readStorage(storeUsersFile);
+
+    if (users.some((user) => user.id === req.params.id)) {
+      let userToDelete = users.find(
+        (user) => user.id === req.params.id
+      ) as User;
+      userToDelete.id = req.params.id;
+
+      if (decodedUser.role == 'admin') {
+        users.splice(users.findIndex((user) => user.id === req.params.id));
+        updateStorage(storeUsersFile, users);
+        res.status(201).send('User deleted.');
+      }
+      if (decodedUser.role == 'user') {
+        if (decodedUser.id == userToDelete.id) {
+          users.splice(users.findIndex((user) => user.id === req.params.id));
+          updateStorage(storeUsersFile, users);
+          res.status(201).send('User deleted.');
+        } else {
+          return res.status(400).send("You can't delete someone's account.");
+        }
+      }
+    } else {
+      return res.status(400).send("User with that id doesn't exist.");
     }
   }
 );
