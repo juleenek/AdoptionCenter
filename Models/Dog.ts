da0c352fa408ccabb6c96c9484659299859f060f
Julia Linek <leeenek0@gmail.com>
2022-06-01 11:57:33 +0200

    Delete comments

--- Models/Dog.ts
+++ Models/Dog.ts
@@ -5,12 +5,10 @@ class Dog {
   public age?: number;
   public gender: string;
   public idCenter: string;
-  // public events: Event[];
 
   constructor(dog: Dog) {
     this.name = dog.name;
     this.breed = dog.breed;
-    // this.events = dog.events;
     this.age = dog.age;
     this.gender = dog.gender;
     this.idCenter = dog.idCenter;
@@ -18,4 +16,3 @@ class Dog {
 }
 export default Dog;
 
-// Czy psy potrzebują Eventów?
